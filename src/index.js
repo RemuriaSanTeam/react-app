61f47c8e425a9b2f59832fe557caa3ab50fde319
remuria <aaa@aaa.com>
2021-10-09 12:10:12 +0900

    コンポーネント追加

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const element=<h1>RemuriaSanTeam</h1>;
+const name='〇〇';
+//変数の埋め込みができる
+const aisatsu=<h1>どうも、{name}さん！</h1>
+
+//{}の中でjsの式を埋め込むことが出来る
+const userName=<img src={name.avatarUrl} />;
+
+//jsxのタグは子要素を持つことが出来る
+const children=(
+  <div>
+    <h3>サイコーでござる</h3>
+    <p>これはたまらん</p>
+  </div>
 );
 
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+ReactDOM.render(
+  aisatsu,
+  document.getElementById('name')
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
