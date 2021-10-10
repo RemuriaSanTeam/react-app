6b5746d494bd1741129ed11a54e017d875cc2218
remuria <aaa@aaa.com>
2021-10-10 22:16:27 +0900

    コンポーネント抽出

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const element=<h1>RemuriaSanTeam</h1>;
-const name='レムリア';
+const element = <h1>RemuriaSanTeam</h1>;
+const name = 'レムリア';
 //変数の埋め込みができる
-const aisatsu=<h1>どうも、{name}さん！</h1>
+const aisatsu = <h1>どうも、{name}さん！</h1>
 
 //{}の中でjsの式を埋め込むことが出来る
 //const userName=<img src={name.avatarUrl} />;
@@ -30,24 +30,24 @@ ReactDOM.render(
   document.getElementById('name')
 );
 
-function tick(){
-  const sample=(
+function tick() {
+  const sample = (
     <div>
       <h2>現在時刻</h2>
       <h2>{new Date().toLocaleTimeString()}です</h2>
     </div>
   );
-  ReactDOM.render(sample,document.getElementById('sample'));
+  ReactDOM.render(sample, document.getElementById('sample'));
 }
-setInterval(tick,1000);
+setInterval(tick, 1000);
 
 //関数コンポーネントの定義(イニシャルの部分は大文字でないとダメ)
-function Welcome(props){
+function Welcome(props) {
   return <h1>どうも{props.name}</h1>;
 }
 //コンポーネントの組み合わせ
-function App(){
-  return(
+function App() {
+  return (
     <div>
       <Welcome name="めそ" />
       <Welcome name="ぽよ" />
@@ -55,12 +55,71 @@ function App(){
     </div>
   );
 }
+
 //Welcome関数のレンダー
 //App関数のレンダー
-const come=<Welcome name="めそ" />;
+const come = <Welcome name="めそ" />;
 ReactDOM.render(
   <App />,
   document.getElementById('function')
+);
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+//コンポーネントの抽出
+function Post(posts) {
+  return (
+    <div className="Comment">
+      <UserInfo user={posts.author} />
+      <div className="Comment-text">
+        {posts.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(posts.date)}
+      </div>
+    </div>
+  );
+}
+
+//Postコンポーネントの分割
+//Avaterを抽出(分割)
+function Avater(posts) {
+  return (
+    <img className="Avater"
+      src={posts.author.avaterUrl}
+      alt={posts.author.name}
+    />
+  );
+}
+//UserInfoの抽出(分割)
+function UserInfo(posts){
+  return(
+    <div className="UserInfo">
+      <Avater user={posts.user} />
+    <div className="UserInfo-name">
+      {posts.author.name}
+    </div>
+  </div>
+  );
+}
+const post = {
+  date: new Date(),
+  text: 'テストテキスト',
+  author: {
+    name: 'レムリア',
+    avaterUrl: '../public/logo192.png',
+  },
+};
+
+ReactDOM.render(
+  <Post
+    date={post.date}
+    text={post.text}
+    author={post.author}
+  />,
+  document.getElementById('post')
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
